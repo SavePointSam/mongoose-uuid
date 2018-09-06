743d45160c031ec7f2db8c52e21c08084e428908
Sam Horton <savepointsam@gmail.com>
2018-09-05 19:11:54 -0700

    chore: remove dead code and update tests

--- test/index.js
+++ test/index.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var bson = require('bson');
 var should = require('chai').should();
 var Schema = mongoose.Schema;
-// Will add the UUID type to the Mongoose Schema types
+
+// adds the UUID type to the Mongoose Schema types
 require('../index')(mongoose);
 var UUID = mongoose.Types.UUID;
 
@@ -17,7 +18,7 @@ ProductSchema.set('toJSON', {getters: true});
 
 var Product = mongoose.model('Product', ProductSchema);
 
-describe('mongoose-uuid', function(){
+describe('mongoose-uuid', function() {
   before(function() {
     return mongoose.connect('mongodb://localhost:27017/mongoose-uuid-test', { useNewUrlParser: true })
   })
@@ -28,20 +29,40 @@ describe('mongoose-uuid', function(){
     });
   });
 
-  it('should cast uuid strings to binary', function(){
+  it('should cast uuid strings to binary', function() {
     var product = new Product({
       _id: '7c401d91-3852-4818-985d-7e7b79f771c3',
       name: 'Some product'
     });
+
     (product._doc._id instanceof mongoose.Types.Buffer.Binary).should.equal(true);
   });
 
-  it('should convert back to text with toObject()', function(){
+  it('should cast null to null', function() {
+    var product = new Product({
+      _id: null,
+      name: 'Some product'
+    });
+
+    (product._doc._id == null).should.equal(true);
+  });
+
+  it('should not throw when given anything other than a Buffer or String', function() {
+    var product = new Product({
+      _id: 300,
+      name: 'Some product'
+    });
+
+    (product._doc._id == null).should.equal(false);
+  });
+
+  it('should convert back to text with toObject()', function() {
     var product = new Product({
       _id: '7c401d91-3852-4818-985d-7e7b79f771c3',
       name: 'Some product'
     });
     var productObject = product.toObject();
+
     (productObject._id).should.equal('7c401d91-3852-4818-985d-7e7b79f771c3');
   });
 
@@ -50,14 +71,18 @@ describe('mongoose-uuid', function(){
       _id: '7c401d91-3852-4818-985d-7e7b79f771c3',
       name: 'Some product'
     });
+
     product.save(cb);
   });
 
   it('should be found correctly with .find()', function(cb) {
     Product.findOne({_id: '7c401d91-3852-4818-985d-7e7b79f771c3'}, function(err, product) {
       (product).should.not.be.null;
+
       var productObject = product.toObject();
+
       (productObject._id).should.equal('7c401d91-3852-4818-985d-7e7b79f771c3');
+
       cb(err);
     });
   });
@@ -65,8 +90,11 @@ describe('mongoose-uuid', function(){
   it('should be found correctly with .findById()', function(cb) {
     Product.findById('7c401d91-3852-4818-985d-7e7b79f771c3', function(err, product) {
       (product).should.not.be.null;
+
       var productObject = product.toObject();
+
       (productObject._id).should.equal('7c401d91-3852-4818-985d-7e7b79f771c3');
+
       cb(err);
     });
   });
@@ -97,11 +125,13 @@ describe('mongoose-uuid', function(){
       var pet = new Pet({
         name: 'Sammy'
       });
+
       var photo = new Photo({
         _id: '7c401d91-3852-4818-985d-7e7b79f771c2',
         filename: 'photo.jpg',
         pet: pet._id
       });
+
       pet.save(function() {
         photo.save(cb);
       })
@@ -111,22 +141,19 @@ describe('mongoose-uuid', function(){
       Pet.deleteMany(function() {
         Photo.deleteMany({}, cb);
       })
-
     });
 
     it('should work', function(cb) {
       Photo.findById('7c401d91-3852-4818-985d-7e7b79f771c2').populate('pet').exec(function(err, photo) {
         (photo.pet).should.exist;
         (photo.pet.name).should.equal('Sammy');
+
         cb(err);
       });
     });
   });
 
   describe('other scenarios', function() {
-    var uuidBuffer = new mongoose.Types.Buffer(uuidv4());
-    uuidBuffer.subtype(bson.Binary.SUBTYPE_UUID);
-
     var BoatSchema = Schema({
       _id: { type: UUID, default: uuidv4 },
       dingy: { type: UUID, ref: 'Boat' },
@@ -145,6 +172,7 @@ describe('mongoose-uuid', function(){
       var boat = new Boat({
         name: 'Sunshine'
       });
+
       (undefined === boat.dingy).should.be.true;
     });
 
@@ -159,15 +187,18 @@ describe('mongoose-uuid', function(){
         name: 'Sunshine',
         dingy: ripples._id
       });
+
       splash.save(function() {
         ripples.save(function() {
           sunshine.save(function() {
             Boat.findOne({name: 'Sunshine'}).populate('dingy').exec(function(err, boat) {
               (boat.dingy._id).should.equal(ripples._id);
+
               boat.dingy = splash._id;
               boat.save(function() {
                 Boat.findOne({name: 'Sunshine'}).populate('dingy').exec(function(err, boat2) {
                   (boat2.dingy._id).should.equal(splash._id);
+
                   cb();
                 })
               })
